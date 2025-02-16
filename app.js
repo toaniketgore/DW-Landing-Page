437a775febb5b04d33f0b8fe5bb398dc61e3c860
Aniket Gore <105165172+toaniketgore@users.noreply.github.com>
2025-02-16 12:50:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ let bee;
 let mixer;
 const loader = new GLTFLoader();
 loader.load(
-  "demon_bee_full_texture.glb",
+  "/demon_bee_full_texture.glb",
   function (gltf) {
     bee = gltf.scene;
     scene.add(bee);
